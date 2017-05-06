9af29cfdd62d53a1611b1e10860aa3fe78dc5a2b
nishaareza <nishaareza@gmail.com>
2017-05-05 20:29:31 -0700

    work in progress

---
+++ bmi.js
@@ -0,0 +1,26 @@
+/**
+ * Created by nisha on 5/5/2017.
+ */
+let firstName   = document.getElementsByName("firstname"),
+    lastName   = document.getElementsByName("lastname"),
+    gender   = document.getElementsByName("gender"),
+    weight   = document.getElementsByName("weight"),
+    height  = document.getElementsByName("height"),
+    age   = document.getElementsByName("age"),
+    waist   = document.getElementsByName("waist"),
+    hip  = document.getElementsByName("hip");
+
+let inputData={
+            "weight": {
+            "value": "85",
+                "unit": "kg"
+        },
+            "height": {
+            "value": "170.00",
+                "unit": "cm"
+        },
+            "sex": "m",
+            "age": "24",
+            "waist": "34.00",
+            "hip": "40.00"
+        }
